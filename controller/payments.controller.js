c871f8dfee6da2725c7f1d8c35a1bd66677cfb08
Saixel <silver.mugiwara@gmail.com>
2021-07-26 11:33:08 +0100

    Add packages purchased

--- controller/payments.controller.js
+++ controller/payments.controller.js
@@ -1,6 +1,15 @@
-const { News } = require('../models/news.model')
+const { User } = require('../models/user.model')
 
 exports.newPayment = (req, res) => {
   console.log('BODY ----->', req.body)
-  console.log('PARAMS ----->', req.params)
-};
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      user.purchasedPacks.push(req.body.product._id)
+      user.save (err => {
+        if (err) return console.error('Error: ', err)
+        res.status(200).json(user)
+      })
+    })
+    .catch((err) => res.status(500).json(err))
+}
